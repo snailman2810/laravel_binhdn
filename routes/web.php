1ff63f95ea770a2e6b88579b187ef88adaaaeda5
Bình Đỗ Nguyễn <binhymhg@gmail.com>
2022-07-28 17:14:49 +0700

    example

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/users', [UserController::class, 'index']);
 
