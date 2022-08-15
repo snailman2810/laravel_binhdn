8ddbec9d1248b1ea2835ada6707c7c7e3e0421b7
Bình Đỗ Nguyễn <binhymhg@gmail.com>
2022-08-15 08:27:03 +0700

    update 15/8

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUpdateUsersTable extends Migration
-{
+class CreateUsersTable extends Migration{
     /**
      * Run the migrations.
      *
@@ -35,4 +34,4 @@ class CreateUpdateUsersTable extends Migration
     {
         Schema::dropIfExists('users');
     }
-};
+}
