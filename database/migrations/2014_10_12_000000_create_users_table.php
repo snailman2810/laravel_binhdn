1509df95882f606c2fc3a5bc6b1b6ff568654ff0
Bình Đỗ Nguyễn <binhymhg@gmail.com>
2022-08-15 08:35:46 +0700

    update 15/8

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration{
+class CreateUsersTable extends Migration
+{
     /**
      * Run the migrations.
      *
@@ -12,8 +13,7 @@ class CreateUsersTable extends Migration{
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table)
-        {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
