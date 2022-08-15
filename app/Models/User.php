b86eb83ca57d248f02510305bd6625b0673474ab
Bình Đỗ Nguyễn <binhymhg@gmail.com>
2022-08-15 08:45:22 +0700

    update 15/8

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Hash;
+
 class User extends Authenticatable
 {
     use HasFactory;
